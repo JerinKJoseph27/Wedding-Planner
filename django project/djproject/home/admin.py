4ec172d0a3e753719071331c94ba4d8ce76b8214
JerinKJoseph27 <jerink.joseph@mca.christuniversity.in>
2026-03-12 18:47:17 +0530

    Refactor Enqry model and admin registration for improved readability and functionality

--- django project/djproject/home/admin.py
+++ django project/djproject/home/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Enqry
-# Register your models here.
 
-admin.site.register(Enqry)
+
+@admin.register(Enqry)
+class EnqryAdmin(admin.ModelAdmin):
+	list_display = ("id", "p_name", "p_email", "p_phone", "p_when", "p_events")
+	search_fields = ("p_name", "p_email", "p_phone", "p_events")
+	list_filter = ("p_events", "p_when")
+	ordering = ("-id",)
--- django project/djproject/home/models.py
+++ django project/djproject/home/models.py
@@ -7,3 +7,6 @@ class Enqry(models.Model):
     p_phone=models.CharField(max_length=14)
     p_when=models.DateField()
     p_events=models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.p_name} - {self.p_events} ({self.p_when})"
