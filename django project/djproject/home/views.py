7c51ab55f398c73edea5b2e024d32ee18db19487
JerinKJoseph27 <jerink.joseph@mca.christuniversity.in>
2026-03-12 15:27:47 +0530

    Enhance enquiry form functionality and improve UI elements

--- django project/djproject/home/views.py
+++ django project/djproject/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import Bookingform
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -9,11 +11,11 @@ def index(request):
         form = Bookingform(request.POST)
         if form.is_valid():
             form.save()
-    form=Bookingform()
-    dict_form={
-        'form' : form
-    }
-    return render(request, 'index.html', dict_form)
+            messages.success(request, "Thank you! Your enquiry has been submitted. We'll get back to you within 24 hours.")
+            return redirect('home')
+    else:
+        form = Bookingform()
+    return render(request, 'index.html', {'form': form})
 
 
 
